fd3342b0c1e90a0a920ed2d1ab488566610eb703
filius-fall <sreeram596@gmail.com>
2021-10-05 22:49:22 +0530

    added storing varaibles to .config folder

---
+++ Hackerjobs/main.py
@@ -0,0 +1,11 @@
+import shutil
+
+def move_file_to_config(path):
+    destination = '/home/filius-fall/.config/hackerjobs'
+    shutil.copy(path,destination)
+
+def env_file_op(api_name,api_version,spread_sheet_id,client_secret_file_name):
+
+    lines = ["SPREAD_SHEET_ID = {0}".format(spread_sheet_id),"API_NAME = {0}".format(api_name),"API_VERSION = {0}".format(api_version),"CLIENT_SECRET_FILE = {0}".format(client_secret_file_name)]
+    with open('.env','w+') as file:
+        file.writelines(lines)
--- Hackerjobs/scripts/hackerjobs_script.py
+++ Hackerjobs/scripts/hackerjobs_script.py
@@ -1,6 +1,23 @@
 import click
 
-@click.command()
-def cli():
-    click.echo('Hello World!')
+from ..main import move_file_to_config,env_file_op
 
+@click.group()
+@click.option('--count', default=1, help='number of greetings')
+def cli(count):
+    click.echo(count)
+
+@cli.command()
+@click.argument('filename')
+def who_wants_to_be_hired(filename):
+    click.echo('Moved')
+    move_file_to_config(filename)
+
+@cli.command()
+@click.option('--api-name',default="",help="insert API-name")
+@click.option('--api-version',default="",help="Takes API-version")
+@click.option('--spread-sheet-id',default="",help="Your Account SpreadSheetId")
+@click.option('--client-secret-file-name',default="",help="Your .json file created during OAth process")
+def config(api_name,api_version,spread_sheet_id,client_secret_file_name):
+    env_file_op(api_name,api_version,spread_sheet_id,client_secret_file_name)
+    click.echo("ENV FILE CREATED")
